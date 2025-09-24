e5151eab0bc41d46e3d500d0c60550f7b580d9c6
D0ubleGi <gigi.matiashvili.king@gmail.com>
2025-09-24 12:27:35 +0400

    a

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ require('dotenv').config({ path: './.env' });
 
 
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
-
 async function uploadToS3(fileName, fileBuffer, mimeType) {
   const params = {
     Bucket: process.env.S3_BUCKET_NAME,
